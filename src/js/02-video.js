648fd787788f8e4f626562481b995cacd9817dcc
Darya-Viunyk <dar.viunyk@gmail.com>
2022-10-14 12:20:43 +0300

    Added task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,25 +1,21 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const localStorageKey = 'videoplayer-current-time';
 
-player.on('timeupdate', _.throttle(updateTime(data), 100));
+const throttleUpdateTime = throttle(updateTime, 1000);
+player.on('timeupdate', throttleUpdateTime);
 
 function updateTime(data) {
  localStorage.setItem(localStorageKey, data.seconds);
 }
-const time = localStorage.getItem(localStorageKey) > 0 ? localStorage.getItem(localStorageKey) : 0;
-player.setCurrentTime(time).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            localStorage.setItem(localStorageKey, 0);
-            break;
 
-        default:
-            // some other error occurred
-            break;
-    }
+let time = localStorage.getItem(localStorageKey);
+time = time ? parseInt(localStorage.getItem(localStorageKey)) : 0;
+player.setCurrentTime(time).then(function(seconds) {})
+  .catch(function(error) {
+    localStorage.setItem(localStorageKey, '0');
 });
 
